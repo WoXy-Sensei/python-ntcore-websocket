623aac75e481f7b7e1d12536a30072da86da2517
Bardia Ramez <bardiagamer8600@gmail.com>
2023-10-31 19:05:59 +0300

    NTcore

--- main.py
+++ main.py
@@ -1,33 +1,28 @@
-import threading
-import signal
-import sys
 import os
-from networktable.NetworkTable import NeworkTable
+from networktable.NetworkTable import NetworkTable
 from websocket.WebSocketServer import WebSocketServer
-from dotenv import load_dotenv
+import threading
+import sys
+import signal
+
+def start_networktable():
+    networktable = NetworkTable(os.getenv("client_name"), os.getenv("host_name"), os.getenv("table_name"))
+    networktable.start()
+
+def start_websocket():
+    websocket = WebSocketServer(os.getenv("ws_host"), os.getenv("ws_port"))
+    websocket.start()
 
 def signal_handler(sig, frame):
-    print("prgoram is closing")
+    print("Closing...")
     sys.exit(0)
 
-
 if __name__ == "__main__":
-    load_dotenv()
-
-    threads = []
-
-    networktable = NeworkTable(os.getenv("client_name"),os.getenv("host_name"),os.getenv("table_name"))
-    websocket = WebSocketServer()
-
-    threads.append(threading.Thread(target= websocket.start ))
-    threads.append(threading.Thread(target= networktable.start ))
-
-    for thread in threads:
-        thread.start()
-
-    for thread in threads:
-        thread.join()
-
-    
     signal.signal(signal.SIGINT, signal_handler)
-    
+
+    networktable_process = threading.Thread(target=start_networktable)
+    websocket_process = threading.Thread(target=start_websocket)
+
+    networktable_process.start()
+    websocket_process.start()
+
